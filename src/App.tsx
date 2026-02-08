699384ea7caa83cfaeb0f2a2f5c9c684ec8e6b8c
Yogesh Kumar <58569752+yogesh0061@users.noreply.github.com>
2026-02-08 13:35:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
-
+//App
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
